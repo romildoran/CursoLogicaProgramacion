64314198bc81a591469f7ebfa5f45f18128349d6
romildoran <ronald260986@gmail.com>
2023-04-10 22:19:19 -0500

    calculadora

--- Calculadora/public_html/js/Programa.js
+++ Calculadora/public_html/js/Programa.js
@@ -1,54 +1,24 @@
-const inputNumbers = document.getElementById("input-numbers");
-const inputResult = document.getElementById("input-result");
-const addBtn = document.getElementById("button-add");
-const subtractBtn = document.getElementById("button-subtract");
-const multiplyBtn = document.getElementById("button-multiply");
-const divideBtn = document.getElementById("button-divide");
-const calculateBtn = document.getElementById("button-calculate");
-let firstNumber = null;
-let selectedOperator = null;
-
-addBtn.addEventListener("click", () => {
-  saveFirstNumber();
-  selectedOperator = "+";
-});
-
-subtractBtn.addEventListener("click", () => {
-  saveFirstNumber();
-  selectedOperator = "-";
-});
-
-multiplyBtn.addEventListener("click", () => {
-  saveFirstNumber();
-  selectedOperator = "*";
-});
-
-divideBtn.addEventListener("click", () => {
-  saveFirstNumber();
-  selectedOperator = "/";
-});
-
-calculateBtn.addEventListener("click", () => {
-  const secondNumber = parseFloat(inputNumbers.value);
-  const result = calculate(firstNumber, selectedOperator, secondNumber);
-  inputResult.value = result;
-});
-
-function saveFirstNumber() {
-  firstNumber = parseFloat(inputNumbers.value);
-  inputNumbers.value = "";
-  inputResult.value = firstNumber;
+function limpiar(){
+  document.getElementById('miFormulario').reset();
 }
 
-function calculate(firstNumber, operator, secondNumber) {
-  switch (operator) {
-    case "+":
-      return firstNumber + secondNumber;
-    case "-":
-      return firstNumber - secondNumber;
-    case "*":
-      return firstNumber * secondNumber;
-    case "/":
-      return firstNumber / secondNumber;
-  }
+function sumar(){
+  var x=parseInt(document.getElementById('valor1').value);
+  var y=parseInt(document.getElementById('valor2').value);
+  document.getElementById('resultado').innerHTML=x+y;
+}
+function restar(){
+  var x=parseInt(document.getElementById('valor1').value);
+  var y=parseInt(document.getElementById('valor2').value);
+  document.getElementById('resultado').innerHTML=x-y;
+}
+function multiplicar(){
+  var x=parseInt(document.getElementById('valor1').value);
+  var y=parseInt(document.getElementById('valor2').value);
+  document.getElementById('resultado').innerHTML=x*y;
+}
+function dividir(){
+  var x=parseInt(document.getElementById('valor1').value);
+  var y=parseInt(document.getElementById('valor2').value);
+  document.getElementById('resultado').innerHTML=x/y;
 }
